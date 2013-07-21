7299e1c24c8cc63d3fbe311db0f9e8684a8f4d2e
Erick Zanardo <erickzanardoo@gmail.com>
2013-07-21 15:48:14 -0300

    Tests improves

--- src/test/java/br/com/ez/DJTest.java
+++ src/test/java/br/com/ez/DJTest.java
@@ -4,17 +4,46 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 
 public class DJTest {
 
 	@Test
-	public void testDJ() {
-		
+	public void testSimpleDJ() {
 		JsonObject object = new JsonObject();
 		object.addProperty("foo", "with doT");
 
-		String result = new DJ().template("<h1>Here is a sample template {{=it.foo}}</h1>").context(object).result();
+		String result = new DJ()
+				.template("<h1>Here is a sample template {{=it.foo}}</h1>")
+				.context(object).result();
 		assertEquals("<h1>Here is a sample template with doT</h1>", result);
 	}
+
+	@Test
+	public void testStringEscapeDJ() {
+		JsonObject object = new JsonObject();
+		object.addProperty("foo", "'\"with doT");
+
+		String result = new DJ()
+				.template("<h1>Here is a sample template {{=it.foo}}</h1>")
+				.context(object).result();
+		assertEquals("<h1>Here is a sample template '\"with doT</h1>", result);
+	}
+
+	@Test
+	public void testArrayEscapeDJ() {
+		JsonObject object = new JsonObject();
+		JsonArray array = new JsonArray();
+		array.add(new JsonPrimitive(1));
+		array.add(new JsonPrimitive(2));
+		object.add("foo", array);
+
+		String result = new DJ()
+				.template("{{~it.foo :value:index}}{{=value}}{{~}}")
+				.context(object).result();
+		assertEquals("12", result);
+	}
+
 }
